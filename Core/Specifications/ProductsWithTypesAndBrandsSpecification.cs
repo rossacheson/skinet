7be91fe7a0cc46b321e491069344a6e421fedc51
Ross Acheson <ross.acheson@gmail.com>
2021-11-08 16:39:07 -0500

    Add search by text

--- Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
+++ Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
@@ -10,7 +10,8 @@ namespace Core.Specifications
     {
         public ProductsWithTypesAndBrandsSpecification(ProductSpecParams productParams)
             : base(x =>
-                (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId)
+                (string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower().Contains(productParams.Search))
+                && (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId)
                 && (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId)
             )
         {
