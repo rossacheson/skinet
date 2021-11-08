e4ce9c29a268b5862104b75d0743ebbfdc0dc5da
Ross Acheson <ross.acheson@gmail.com>
2021-11-08 15:31:48 -0500

    Return paged results based on ProductSpecParams class

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -37,9 +37,10 @@ namespace API.Controllers
 
         [HttpGet]
         public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts(
-            string sort, int? brandId, int? typeId)
+            [FromQuery]ProductSpecParams productParams
+        )
         {
-            var spec = new ProductsWithTypesAndBrandsSpecification(sort, brandId, typeId);
+            var spec = new ProductsWithTypesAndBrandsSpecification(productParams);
             var products = await _productsRepo.ListAsync(spec);
             return Ok(_mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products));
         }
