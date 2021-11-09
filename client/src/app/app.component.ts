7abd5a973e6898aab57e1e1447a03ac6a759f38f
Ross Acheson <ross.acheson@gmail.com>
2021-11-09 11:56:19 -0500

    Fetch and display products

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Skinet';
+export class AppComponent implements OnInit {
+  public title = 'Skinet';
+  public products: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products').subscribe((response: any) => {
+      console.log(response);
+      this.products = response.data;
+    }, error => {
+      console.log(error);
+    });
+  }
 }
