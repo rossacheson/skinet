6e75ab0242623bcfc463b916a4e4dd7a7245c967
Ross Acheson <rossacheson@users.noreply.github.com>
2022-08-05 14:07:40 -0500

    Add section header and breadcrumbs

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ToastrModule } from 'ngx-toastr';
+import { BreadcrumbModule } from 'xng-breadcrumb';
 
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { TestErrorComponent } from './test-error/test-error.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ServerErrorComponent } from './server-error/server-error.component';
+import { SectionHeaderComponent } from './section-header/section-header.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +16,7 @@ import { ServerErrorComponent } from './server-error/server-error.component';
     TestErrorComponent,
     NotFoundComponent,
     ServerErrorComponent,
+    SectionHeaderComponent,
   ],
   imports: [
     CommonModule,
@@ -22,7 +25,8 @@ import { ServerErrorComponent } from './server-error/server-error.component';
       positionClass: 'toast-bottom-right',
       preventDuplicates: true,
     }),
+    BreadcrumbModule,
   ],
-  exports: [NavBarComponent],
+  exports: [NavBarComponent, SectionHeaderComponent],
 })
 export class CoreModule {}
