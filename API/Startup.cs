51ec294479814515b2ed814ae763887815f8207c
Ross Acheson <rossacheson@users.noreply.github.com>
2022-08-09 11:41:15 -0500

    Basket API and Redis server via Docker

--- API/Startup.cs
+++ API/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using StackExchange.Redis;
 
 namespace API
 {
@@ -25,8 +26,13 @@ namespace API
             services.AddAutoMapper(typeof(MappingProfiles));
             services.AddControllers();
             services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
-            
-            services.AddApplicationServices();
+
+      services.AddSingleton<IConnectionMultiplexer>(c => {
+        var configuration = ConfigurationOptions.Parse(_config.GetConnectionString("Redis"), true);
+        return ConnectionMultiplexer.Connect(configuration);
+      });
+
+      services.AddApplicationServices();
             services.AddSwaggerDocumentation();
             services.AddCors(opt => 
             {
