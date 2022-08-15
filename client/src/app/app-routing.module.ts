39a9f64e80bda25fb1eb3708c395f843a09d906c
Ross Acheson <rossacheson@users.noreply.github.com>
2022-08-15 11:19:18 -0500

    Add Account module

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -43,6 +43,12 @@ const routes: Routes = [
       ),
     data: { breadcrumb: 'Checkout' },
   },
+  {
+    path: 'account',
+    loadChildren: () =>
+      import('./account/account.module').then((module) => module.AccountModule),
+    data: { breadcrumb: { skip: true } },
+  },
   { path: '**', redirectTo: 'not-found', pathMatch: 'full' },
 ];
 
