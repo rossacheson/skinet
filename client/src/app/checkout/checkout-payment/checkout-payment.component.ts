2c14c07254cf321eef2537a93426927284efca62
Ross Acheson <rossacheson@users.noreply.github.com>
2022-08-24 15:56:28 -0500

    Payment and success components

--- client/src/app/checkout/checkout-payment/checkout-payment.component.ts
+++ client/src/app/checkout/checkout-payment/checkout-payment.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { NavigationExtras, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+
+import { BasketService } from 'src/app/basket/basket.service';
+import { IBasket } from 'src/app/shared/models/basket';
+import { IOrder } from 'src/app/shared/models/order';
+import { CheckoutService } from '../checkout.service';
 
 @Component({
   selector: 'app-checkout-payment',
   templateUrl: './checkout-payment.component.html',
-  styleUrls: ['./checkout-payment.component.scss']
+  styleUrls: ['./checkout-payment.component.scss'],
 })
 export class CheckoutPaymentComponent implements OnInit {
+  @Input() checkoutForm!: FormGroup;
 
-  constructor() { }
+  constructor(
+    private basketService: BasketService,
+    private checkoutService: CheckoutService,
+    private toastr: ToastrService,
+    private router: Router
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  submitOrder(): void {
+    const basket = this.basketService.getCurrentBasketValue()!;
+    const orderToCreate = this.getOrderToCreate(basket);
+    this.checkoutService.createOrder(orderToCreate).subscribe({
+      next: (order) => {
+        this.toastr.success('Order created successfully');
+        this.basketService.deleteLocalBasket(basket.id);
+        const navigationExtras: NavigationExtras = { state: order };
+        this.router.navigate(['checkout/success'], navigationExtras);
+      },
+      error: (err: any) => console.log(err),
+    });
   }
 
+  private getOrderToCreate(basket: IBasket) {
+    return {
+      basketId: basket.id,
+      deliveryMethodId: +this.checkoutForm
+        .get('deliveryForm')!
+        .get('deliveryMethod')!.value,
+      shipToAddress: this.checkoutForm.get('addressForm')!.value,
+    };
+  }
 }
--- client/src/app/checkout/checkout.service.ts
+++ client/src/app/checkout/checkout.service.ts
@@ -1,9 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+
 import { environment } from 'src/environments/environment';
 import { IDeliveryMethod } from '../shared/models/deliveryMethod';
-import { IOrderToCreate } from '../shared/models/order';
+import { IOrder, IOrderToCreate } from '../shared/models/order';
 
 @Injectable({
   providedIn: 'root',
@@ -13,8 +15,8 @@ export class CheckoutService {
 
   constructor(private http: HttpClient) {}
 
-  createOrder(order: IOrderToCreate) {
-    return this.http.post(this.baseUrl + 'orders', order);
+  createOrder(order: IOrderToCreate): Observable<IOrder> {
+    return this.http.post<IOrder>(this.baseUrl + 'orders', order);
   }
 
   getDeliveryMethods() {
